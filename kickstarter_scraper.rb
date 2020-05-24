5ea4d6c23f531c5e230776ab52589e1306fc2cb2
DAZUN XING <dazun.xing@gmail.com>
2020-05-24 08:55:00 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,8 +12,8 @@ def create_project_hash
     projects[title.to_sym] = {
       :image_link => project.css("a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
-      :location => project.css("ul.project-meta span.location-name").text,
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+      :location => project.css("span.location-name").text,
+      :percent_funded => project.css("li.first.funded strong").text.gsub("%","").to_i
     }
   end
  
